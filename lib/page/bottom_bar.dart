ae1c766e1773336bc971cc28837a81025a2e5879
jaehee21232 <a62533196@gmail.com>
2023-01-08 05:46:13 +0900

    바템 네비게이션바 디자인

--- lib/page/bottom_bar.dart
+++ lib/page/bottom_bar.dart
@@ -2,9 +2,10 @@ import 'package:code_dc/model/color.dart';
 import 'package:flutter/material.dart';
 
 class BottomBar extends StatefulWidget {
-  BottomBar({super.key, required this.selectedIndex});
+  BottomBar(
+      {super.key, required this.selectedIndex, required this.pageController});
   int selectedIndex;
-
+  PageController pageController;
   @override
   State<BottomBar> createState() => _BottomBarState();
 }
@@ -13,43 +14,60 @@ class _BottomBarState extends State<BottomBar> {
   void _onItemTapped(int item) {
     setState(() {
       widget.selectedIndex = item;
+      widget.pageController.jumpToPage(item);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-        backgroundColor: DCColor.backgroundcolor,
-        type: BottomNavigationBarType.fixed,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        currentIndex: widget.selectedIndex,
-        selectedItemColor: DCColor.dcyellow,
-        unselectedItemColor: Colors.white,
-        onTap: (value) {
-          _onItemTapped(value);
-        },
-        items: const [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.abc),
-              label: "a",
-              backgroundColor: DCColor.backgroundcolor),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.abc),
-              label: "c",
-              backgroundColor: DCColor.backgroundcolor),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.abc),
-              label: "c",
-              backgroundColor: DCColor.backgroundcolor),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.abc),
-              label: "c",
-              backgroundColor: DCColor.backgroundcolor),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.abc),
-              label: "c",
-              backgroundColor: DCColor.backgroundcolor)
-        ]);
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(top: BorderSide(color: Colors.white, width: 0.1)),
+      ),
+      child: BottomNavigationBar(
+          backgroundColor: DCColor.backgroundcolor,
+          type: BottomNavigationBarType.fixed,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          currentIndex: widget.selectedIndex,
+          selectedItemColor: DCColor.dcyellow,
+          unselectedItemColor: Colors.white,
+          enableFeedback: false,
+          onTap: (value) {
+            _onItemTapped(value);
+          },
+          iconSize: 30,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(widget.selectedIndex == 0
+                    ? Icons.home
+                    : Icons.home_outlined),
+                label: "a",
+                tooltip: "",
+                backgroundColor: DCColor.backgroundcolor),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.abc),
+                label: "c",
+                tooltip: "",
+                backgroundColor: DCColor.backgroundcolor),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.abc),
+                label: "c",
+                tooltip: "",
+                backgroundColor: DCColor.backgroundcolor),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.abc),
+                label: "c",
+                tooltip: "",
+                backgroundColor: DCColor.backgroundcolor),
+            BottomNavigationBarItem(
+                icon: Icon(widget.selectedIndex == 4
+                    ? Icons.account_circle_sharp
+                    : Icons.account_circle),
+                label: "c",
+                tooltip: "",
+                backgroundColor: DCColor.backgroundcolor)
+          ]),
+    );
   }
 }
