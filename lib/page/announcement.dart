dee5a99909b845242d3bbae4bda3163aaa0bfcda
jaehee21232 <a62533196@gmail.com>
2023-01-08 05:45:57 +0900

    공지사항 페이지 추가

---
+++ lib/page/announcement.dart
@@ -0,0 +1,31 @@
+import 'package:code_dc/model/color.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'package:flutter/material.dart';
+
+class AnnouncementPage extends StatelessWidget {
+  const AnnouncementPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: SingleChildScrollView(
+          child: Container(
+        height: size.height,
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "동아리 공지사항",
+              style: TextStyle(color: Colors.white, fontSize: 30),
+            )
+          ],
+        ),
+      )),
+    );
+  }
+}
