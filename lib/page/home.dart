5725f463ef8d7d2a4f58edfc6f406f5ce1255876
jaehee21232 <a62533196@gmail.com>
2023-01-08 05:46:21 +0900

    홈 추가

---
+++ lib/page/home.dart
@@ -0,0 +1,30 @@
+import 'package:code_dc/model/color.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: SingleChildScrollView(
+          child: Container(
+        height: size.height,
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "홈 화면",
+              style: TextStyle(color: Colors.white, fontSize: 30),
+            )
+          ],
+        ),
+      )),
+    );
+  }
+}
