112626f0718b6ab55b13245e4679fe91afbb155d
jaehee21232 <a62533196@gmail.com>
2023-01-09 01:37:40 +0900

    feat: 폰트사이즈 함수로 받는 기능 추가

--- lib/model/color.dart
+++ lib/model/color.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/animation.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class DCColor {
   static const Color backgroundcolor = Color.fromARGB(255, 13, 17, 23);
   static const Color dcyellow = Color.fromARGB(255, 255, 200, 0);
+  boldFontBlack(double fontsize) {
+    return TextStyle(
+        fontSize: fontsize,
+        fontFamily: "urbanist",
+        fontWeight: FontWeight.w600,
+        color: Colors.black);
+  }
+
+  boldFontWhite(double fontsize) {
+    return TextStyle(
+        fontSize: fontsize,
+        fontFamily: "urbanist",
+        fontWeight: FontWeight.w600,
+        color: Colors.white);
+  }
 }
