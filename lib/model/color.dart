5dcecf6f55161b6089cd5ba751312537bc6f0e83
jaehee21232 <a62533196@gmail.com>
2023-01-08 20:55:48 +0900

    feat: 백그라운드 색 변경

--- lib/model/color.dart
+++ lib/model/color.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/animation.dart';
 
 class DCColor {
-  static const Color backgroundcolor = Color(0x000D1117);
+  static const Color backgroundcolor = Color.fromARGB(255, 13, 17, 23);
   static const Color dcyellow = Color.fromARGB(255, 255, 200, 0);
 }
