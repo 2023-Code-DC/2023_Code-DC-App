3bc9fa6b7b6f71a99050e7e4b640a4b01acd7175
jaehee21232 <a62533196@gmail.com>
2023-01-08 19:38:14 +0900

    색 변경

--- lib/model/color.dart
+++ lib/model/color.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/animation.dart';
 
 class DCColor {
-  static const Color backgroundcolor = Color(0x00000000);
+  static const Color backgroundcolor = Color(0x000D1117);
   static const Color dcyellow = Color.fromARGB(255, 255, 200, 0);
 }
