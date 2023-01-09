4161036efd02930f999789fced2e689df9e4ec24
jaehee21232 <a62533196@gmail.com>
2023-01-09 21:32:31 +0900

    feat: 라우터 기능 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,38 @@
+import 'package:code_dc/loading/main_loading.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'HomeScreen.dart';
+import 'package:page_transition/page_transition.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: HomeScreen(),
-    theme: ThemeData(
-        textTheme: TextTheme(
-            bodyText1: TextStyle(color: Colors.white),
-            bodyText2: TextStyle(color: Colors.white))),
-  ));
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+      theme: ThemeData(
+          textTheme: TextTheme(
+              bodyText1: TextStyle(color: Colors.white),
+              bodyText2: TextStyle(color: Colors.white))),
+      routes: {
+        '/test': (BuildContext context) => TestPage(),
+      },
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case '/mainloadingpage':
+            return PageTransition(
+              child: MainLoadingPage(),
+              type: PageTransitionType.fade,
+              settings: settings,
+            );
+          default:
+            return null;
+        }
+      },
+    ),
+  );
 }
