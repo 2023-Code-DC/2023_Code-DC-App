9e81d01ea81e5508f24f41e4579f5a1cd55fd240
jaehee21232 <a62533196@gmail.com>
2023-01-09 21:31:14 +0900

    feat: 앱 맨처음 화면 추가, 버튼 누르면 페이지 넘어가기 추가

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,117 +1,51 @@
+import 'package:code_dc/loading/main_loading.dart';
 import 'package:code_dc/model/color.dart';
-import 'package:code_dc/model/dcfirestore.dart';
-import 'package:code_dc/page/main_page.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:http/http.dart' as http;
+import 'package:page_transition/page_transition.dart';
 
-GoogleSignIn _googleSignIn = GoogleSignIn(
-  // Optional clientId
-  // clientId: '479882132969-9i9aqik3jfjd7qhci1nqf0bm2g71rm1u.apps.googleusercontent.com',
-  scopes: <String>[
-    'email',
-    'https://www.googleapis.com/auth/contacts.readonly',
-  ],
-);
-
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  GoogleSignInAccount? _currentUser;
-  @override
-  void initState() {
-    super.initState();
-    _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount? account) {
-      //현재 사용자가 변경 될때 실행됨
-      setState(() {
-        _currentUser = account;
-      });
-      if (_currentUser != null) {
-        _handleGetContact(_currentUser!);
-      }
-    });
-    _googleSignIn.signInSilently();
-    //이전에 인증된 사용자로 로그인 시도
-  }
-
-  Future<void> _handleGetContact(GoogleSignInAccount user) async {
-    final http.Response response = await http.get(
-      Uri.parse('https://people.googleapis.com/v1/people/me/connections'
-          '?requestMask.includeField=person.names'),
-      headers: await user.authHeaders,
-    );
-    if (response.statusCode != 200) {
-      setState(() {
-        print('People API gave a ${response.statusCode} '
-            'response. Check logs for details.');
-        //오류 메시지
-      });
-      print('People API ${response.statusCode} response: ${response.body}');
-      return;
-    }
-  }
-
-  Future<UserCredential> signInWithGoogle() async {
-    // Trigger the authentication flow
-    final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
-
-    // Obtain the auth details from the request
-    final GoogleSignInAuthentication? googleAuth =
-        await googleUser?.authentication;
-
-    // Create a new credential
-    final credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth?.accessToken,
-      idToken: googleAuth?.idToken,
-    );
-
-    // Once signed in, return the UserCredential
-    return await FirebaseAuth.instance.signInWithCredential(credential);
-  }
-
-  Future<void> SignOutWithGoogle() => _googleSignIn.disconnect();
-  //로그아웃
-
-  @override
   Widget build(BuildContext context) {
-    return _currentUser != null
-        ? MainPage(
-            SignOut: SignOutWithGoogle,
-            user: _currentUser,
-          )
-        : Scaffold(
-            backgroundColor: DCColor.backgroundcolor,
-            body: Center(
-                child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  child: Text("가입"),
-                  onPressed: (() {
-                    signInWithGoogle();
-                  }),
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                Text(
+                  "동무!",
+                  style: DCColor().boldFontBlack(30),
                 ),
-                ElevatedButton(
+                Text(
+                  "동무를 찾고있었습네다.",
+                  style: DCColor().boldFontBlack(30),
+                ),
+                Text("발신 -코드디씨-", style: DCColor().boldFontBlack(16)),
+              ]),
+              Container(
+                width: double.infinity,
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8)),
+                        minimumSize: Size(330, 56),
+                        backgroundColor: DCColor.backgroundcolor),
                     onPressed: (() {
-                      SignOutWithGoogle();
+                      Navigator.popAndPushNamed(context, "/mainloadingpage");
                     }),
-                    child: Text("로그아웃")),
-                ElevatedButton(
-                    onPressed: (() {
-                      print('name = ${_currentUser?.displayName}');
-                      print('email = ${_currentUser?.email}');
-                      print('id = ${_currentUser?.id}');
-                      print('id = ${_currentUser?.photoUrl}');
-                    }),
-                    child: Text("확인"))
-              ],
-            )),
-          );
+                    child: Text(
+                      "단추 눌러 동무 되기",
+                      style: DCColor().boldFontWhite(15),
+                    )),
+              )
+            ]),
+      ),
+    );
   }
 }
