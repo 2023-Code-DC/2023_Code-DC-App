7d47aff2ebc3ae5c090a125a239c7bcd0020d762
jaehee21232 <a62533196@gmail.com>
2023-01-10 01:26:50 +0900

    feat: 이름변경, 라우터 이름 변경

--- lib/loading/main_loading.dart
+++ lib/loading/main_loading.dart
@@ -1,9 +1,12 @@
 import 'package:animations/animations.dart';
+import 'package:code_dc/login/login.dart';
 import 'package:code_dc/model/color.dart';
+import 'package:code_dc/page/main_page.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'dart:async';
 import 'package:animations/animations.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class MainLoadingPage extends StatefulWidget {
   const MainLoadingPage({super.key});
@@ -43,7 +46,7 @@ class _MainLoadingPageState extends State<MainLoadingPage> {
     });
     Timer(Duration(milliseconds: 3850), () {
       setState(() {
-        Navigator.popAndPushNamed(context, "/test");
+        Navigator.popAndPushNamed(context, "/choice");
       });
     });
   }
@@ -126,31 +129,39 @@ class _MainLoadingPageState extends State<MainLoadingPage> {
   }
 }
 
-class TestPage extends StatelessWidget {
-  const TestPage({super.key});
+class ChoicePage extends StatelessWidget {
+  ChoicePage({super.key, required this.user, required this.SignOut});
+  final SignOut;
+  GoogleSignInAccount? user;
 
   @override
   Widget build(BuildContext context) {
+    var ang = Center(
+        child: Column(
+      children: [
+        ElevatedButton(
+            onPressed: (() {
+              Navigator.pop(context);
+            }),
+            child: Text("pop")),
+        Text(
+          "테스트 페이지 현재 뒤로가기 막음",
+          style: DCColor().boldFontBlack(20),
+        ),
+        Text(
+          user != null ? user!.displayName.toString() : "없음",
+          style: DCColor().boldFontBlack(20),
+        )
+      ],
+    ));
     return WillPopScope(
       onWillPop: (() {
         return Future(() => false);
       }),
       child: Scaffold(
-        body: Center(
-            child: Column(
-          children: [
-            ElevatedButton(
-                onPressed: (() {
-                  Navigator.pop(context);
-                }),
-                child: Text("pop")),
-            Text(
-              "테스트 페이지 현재 뒤로가기 막음",
-              style: DCColor().boldFontBlack(20),
-            ),
-          ],
-        )),
-      ),
+          body: user != null
+              ? MainPage(SignOut: SignOut, user: user)
+              : LoginPage()),
     );
   }
 }
