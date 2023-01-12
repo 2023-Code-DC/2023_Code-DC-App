0d922a9b599c260082dc36eaf61fa000d0c170b3
jaehee21232 <a62533196@gmail.com>
2023-01-12 19:37:55 +0900

    feat: 로딩페이지 추가

---
+++ lib/loading/circular_loading.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class CircularLoadingPage extends StatelessWidget {
+  const CircularLoadingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [CircularLoadingPage()]),
+    );
+  }
+}
