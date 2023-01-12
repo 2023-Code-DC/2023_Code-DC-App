f93cad821cb8c86294997cceab16bd210597923e
jaehee21232 <a62533196@gmail.com>
2023-01-12 19:40:48 +0900

    fix: GoogleSignInAccount  로 받아서 힘들었던거 User?로 받음

--- lib/model/dcfirestore.dart
+++ lib/model/dcfirestore.dart
@@ -1,26 +1,27 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 
 final firestore = FirebaseFirestore.instance;
 
 class UserData {
-  getData(GoogleSignInAccount user, String data) async {
-    var result = await firestore.collection('userdata').doc(user.id).get();
+  getData(User? user, String data) async {
+    var result = await firestore.collection('userdata').doc(user!.uid).get();
     print(result["email"]);
     print(result["image"]);
     print(result["image"].runtimeType);
     return result["image"];
   }
 
-  addData(GoogleSignInAccount user) async {
+  addData(User? user) async {
     final userdata = {
-      "displayName": user.displayName.toString(),
+      "displayName": user!.displayName.toString(),
       "name": user.displayName.toString(),
       "email": user.email,
-      "image": user.photoUrl != null ? user.photoUrl.toString() : "none"
+      "image": user.photoURL != null ? user.photoURL.toString() : "none"
     };
-    firestore.collection("userdata").doc(user.id).set(userdata);
+    firestore.collection("userdata").doc(user.uid).set(userdata);
   }
 
   DeviceAddData(
@@ -29,16 +30,13 @@ class UserData {
     String email,
     String image,
   ) async {
-    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
-    AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
-    print('Running on ${androidInfo.model}');
-    print(androidInfo.id);
+    User? user = FirebaseAuth.instance.currentUser;
     final userdata = {
       "displayName": displayName,
       "name": displayName,
       "email": email,
       "image": image
     };
-    firestore.collection("userdata").doc(androidInfo.id).set(userdata);
+    firestore.collection("userdata").doc(user?.uid).set(userdata);
   }
 }
