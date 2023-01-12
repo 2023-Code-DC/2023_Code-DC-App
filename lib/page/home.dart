4354bbe754fdb4fc8af1bcdb8c9f3e97d5baae54
jaehee21232 <a62533196@gmail.com>
2023-01-12 21:04:38 +0900

    feat: 임시 데이터 확인가능한 버튼

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,11 +1,18 @@
 import 'package:code_dc/model/color.dart';
+import 'package:code_dc/model/login_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   const Homepage({super.key});
 
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -21,7 +28,20 @@ class Homepage extends StatelessWidget {
             Text(
               "홈 화면",
               style: TextStyle(color: Colors.white, fontSize: 30),
-            )
+            ),
+            ElevatedButton(
+                onPressed: (() {
+                  UserAuthentication().SignOutWithGoogle();
+                  setState(() {});
+                }),
+                child: Text("로그아웃")),
+            ElevatedButton(
+                onPressed: (() {
+                  final _authentication = FirebaseAuth.instance.currentUser;
+                  print(_authentication);
+                  setState(() {});
+                }),
+                child: Text("현재상태"))
           ],
         ),
       )),
