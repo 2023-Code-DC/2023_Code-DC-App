a30c0f6bc3efcc4d2aa63f20274e2cacb8e01fa5
jaehee21232 <a62533196@gmail.com>
2023-01-12 15:37:57 +0900

    feat: 스낵바 추가, 회원가입시 디바이스id 여기서 받는걸로 기능 추가

--- lib/model/login_model.dart
+++ lib/model/login_model.dart
@@ -1,7 +1,10 @@
+import 'package:code_dc/model/color.dart';
+import 'package:code_dc/model/dcfirestore.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:http/http.dart' as http;
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 
+final _authentication = FirebaseAuth.instance;
 GoogleSignIn _googleSignIn = GoogleSignIn(
   // Optional clientId
   // clientId: '479882132969-9i9aqik3jfjd7qhci1nqf0bm2g71rm1u.apps.googleusercontent.com',
@@ -10,8 +13,13 @@ GoogleSignIn _googleSignIn = GoogleSignIn(
     'https://www.googleapis.com/auth/contacts.readonly',
   ],
 );
+final Already = SnackBar(
+  backgroundColor: DCColor.backgroundcolor,
+  content: Text("이미 존재하는 전자우편이라우 동무", style: DCColor().boldFontWhite(20)),
+  duration: Duration(seconds: 2),
+);
 
-class GoogleUser {
+class UserAuthentication {
   Future<UserCredential> signInWithGoogle() async {
     // Trigger the authentication flow
     final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
@@ -31,4 +39,19 @@ class GoogleUser {
   }
 
   Future<void> SignOutWithGoogle() => _googleSignIn.disconnect();
+
+  siginInWithDevice(BuildContext context, String username, String userEmail,
+      String userPassword) async {
+    try {
+      final newUser = await _authentication.createUserWithEmailAndPassword(
+          email: userEmail, password: userPassword);
+      UserData().DeviceAddData(username, username, userEmail, "none");
+    } on FirebaseAuthException catch (e) {
+      if (e.code == "email-already-in-use") {
+        ScaffoldMessenger.of(context).showSnackBar(Already);
+      } else {
+        print(e);
+      }
+    }
+  }
 }
