a4fd0dd8dfbb69f5d15daee1c3509ab037d95093
jaehee21232 <a62533196@gmail.com>
2023-01-15 11:37:44 +0900

    feat: 계정페이지 메뉴 기능 추가

--- lib/page/account.dart
+++ lib/page/account.dart
@@ -1,4 +1,5 @@
 import 'package:code_dc/model/color.dart';
+import 'package:code_dc/model/login_model.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -42,38 +43,93 @@ class _AccountPageState extends State<AccountPage> {
         if (snapshot.hasData) {
           return SafeArea(
               child: Padding(
-            padding: const EdgeInsets.fromLTRB(4.0, 0, 4.0, 0),
-            child: Container(
-              width: double.infinity,
-              height: size.height,
-              child: Column(children: [
-                ElevatedButton(
-                    onPressed: (() {
-                      print(user);
-                    }),
-                    child: Text("ㅁㄴㅇ")),
-                CircleAvatar(
-                  radius: 40,
-                  backgroundImage: avatorImage(),
-                ),
-                const SizedBox(
-                  width: 1,
-                  height: 10,
-                ),
-                Column(
-                  children: [
-                    Text(
-                      result["name"].toString(),
-                      style: DCColor().boldFontWhite(20),
-                    ),
-                    Text(user!.email.toString(),
-                        style: DCColor().boldFontWhite(20))
-                  ],
-                ),
-                Drawer()
-              ]),
-            ),
-          ));
+                  padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          IconButton(
+                              onPressed: (() {
+                                showModalBottomSheet<void>(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return Container(
+                                        decoration: BoxDecoration(
+                                          color: DCColor.backgroundcolor,
+                                        ),
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(32.0),
+                                          child: Column(
+                                            children: [
+                                              Container(
+                                                width: 50,
+                                                height: 5,
+                                                decoration: BoxDecoration(
+                                                    color: Colors.white,
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            10)),
+                                              ),
+                                              SizedBox(
+                                                height: 20,
+                                                width: 1,
+                                              ),
+                                              Text('메뉴',
+                                                  textAlign: TextAlign.center,
+                                                  style: DCColor()
+                                                      .boldFontWhite(20)),
+                                              ElevatedButton(
+                                                  onPressed: (() {
+                                                    UserAuthentication()
+                                                        .SignOutWithGoogle(
+                                                            context);
+                                                    setState(() {});
+                                                  }),
+                                                  child: Text("로그아웃")),
+                                            ],
+                                          ),
+                                        ),
+                                      );
+                                    });
+                              }),
+                              icon: Icon(
+                                Icons.menu,
+                                color: Colors.white,
+                                size: 30,
+                              ))
+                        ],
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        child: Column(children: [
+                          SizedBox(
+                            width: 10,
+                            height: size.height * 0.065,
+                          ),
+                          CircleAvatar(
+                            radius: 40,
+                            backgroundImage: avatorImage(),
+                          ),
+                          const SizedBox(
+                            width: 1,
+                            height: 10,
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                result["name"].toString(),
+                                style: DCColor().boldFontWhite(20),
+                              ),
+                              Text(user!.email.toString(),
+                                  style: DCColor().boldFontWhite(20)),
+                              Text("해킹보안과", style: DCColor().boldFontWhite(20))
+                            ],
+                          ),
+                        ]),
+                      ),
+                    ],
+                  )));
         } else {
           return const Center(
             child: CircularProgressIndicator(
