6faed3cadc550c6ac094d2008acc32c0c5d1815a
jaehee21232 <a62533196@gmail.com>
2023-01-18 20:37:03 +0900

    feat: 페이지 이동 추가

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,5 +1,6 @@
 import 'package:code_dc/model/color.dart';
 import 'package:code_dc/model/dcfirestore.dart';
+import 'package:code_dc/page/application.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'dart:async';
@@ -170,10 +171,9 @@ class _HomepageState extends State<Homepage> {
                                     Size(size.width * 0.3, size.width * 0.06),
                                 backgroundColor: DCColor.dcyellow,
                                 shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10))),
+                                    borderRadius: BorderRadius.circular(8))),
                             onPressed: (() {
-                              print(size.height);
-                              print(size.width);
+                              Navigator.pushNamed(context, "/applicationpage");
                             }),
                             child: Center(
                                 child: Text(
