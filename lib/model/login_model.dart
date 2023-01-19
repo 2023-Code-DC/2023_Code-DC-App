fee0b6ec597479ab7641534d5eafa3e972d3c9de
jaehee21232 <a62533196@gmail.com>
2023-01-19 15:35:39 +0900

    fix: 구글 로그인시 문서 바뀌는 버그 고침

--- lib/model/login_model.dart
+++ lib/model/login_model.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:code_dc/model/color.dart';
 import 'package:code_dc/model/dcfirestore.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -5,7 +6,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-final storage = new FlutterSecureStorage();
+const storage = FlutterSecureStorage();
 GoogleSignIn _googleSignIn = GoogleSignIn(
   // Optional clientId
   // clientId: '479882132969-9i9aqik3jfjd7qhci1nqf0bm2g71rm1u.apps.googleusercontent.com',
@@ -18,25 +19,25 @@ final Already = SnackBar(
   backgroundColor: DCColor.gitcolor,
   content: Text("이미 존재하는 전자우편이라우 동무",
       style: DCColor().boldFontWhite(20, FontWeight.w600)),
-  duration: Duration(seconds: 2),
+  duration: const Duration(seconds: 2),
 );
 final UserNotFound = SnackBar(
   backgroundColor: DCColor.gitcolor,
   content: Text("동무는 당원신청서를 작성하라우",
       style: DCColor().boldFontWhite(20, FontWeight.w600)),
-  duration: Duration(seconds: 2),
+  duration: const Duration(seconds: 2),
 );
 final WrongPassword = SnackBar(
   backgroundColor: DCColor.gitcolor,
   content: Text("동무 통과암호가 틀리다우",
       style: DCColor().boldFontWhite(20, FontWeight.w600)),
-  duration: Duration(seconds: 2),
+  duration: const Duration(seconds: 2),
 );
 final InvalidEmail = SnackBar(
   backgroundColor: DCColor.gitcolor,
   content:
       Text("동무 이메일이 틀리다우", style: DCColor().boldFontWhite(20, FontWeight.w600)),
-  duration: Duration(seconds: 2),
+  duration: const Duration(seconds: 2),
 );
 
 class UserAuthentication {
@@ -58,23 +59,25 @@ class UserAuthentication {
 
     return await FirebaseAuth.instance
         .signInWithCredential(credential)
-        .then((value) {
+        .then((value) async {
       Navigator.popAndPushNamed(context, "/mainpage");
-      UserData().firstData();
+      DocumentSnapshot<Map<String, dynamic>> result = await firestore
+          .collection('userdata')
+          .doc(user!.uid)
+          .get()
+          .then(
+              (value) => value.exists == true ? null : UserData().firstData());
     });
   }
 
   SignOutWithGoogle(BuildContext context) async {
-    final _authentication = FirebaseAuth.instance;
+    final authentication = FirebaseAuth.instance;
     String? logindata = await storage.read(key: 'login');
     if (logindata == "device") {
-      _authentication.signOut();
+      authentication.signOut();
     } else {
       _googleSignIn.disconnect();
     }
-    print(logindata);
-    print(_authentication);
-    print(_googleSignIn);
     try {
       Navigator.popAndPushNamed(context, "/homescreen");
     } catch (e) {
@@ -86,9 +89,9 @@ class UserAuthentication {
 
   CreateWithDevice(BuildContext context, String username, String userEmail,
       String userPassword) async {
-    final _authentication = FirebaseAuth.instance;
+    final authentication = FirebaseAuth.instance;
     try {
-      final newUser = await _authentication.createUserWithEmailAndPassword(
+      final newUser = await authentication.createUserWithEmailAndPassword(
           email: userEmail, password: userPassword);
       UserData().DeviceAddData(username, username, userEmail, "none");
       Navigator.pop(context);
