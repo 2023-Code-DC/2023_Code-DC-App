ba22ab0ba326311fe9a5d7c3b2f6ba009b7440a3
jaehee21232 <a62533196@gmail.com>
2023-01-21 17:29:39 +0900

    feat: 코드줄이기용 모델 데코레이션 추가

--- lib/model/color.dart
+++ lib/model/color.dart
@@ -45,4 +45,58 @@ class DCColor {
         fontWeight: w,
         color: const Color.fromRGBO(255, 208, 41, 1));
   }
+
+  textFieldDecoration(String text) {
+    return InputDecoration(
+        contentPadding: const EdgeInsets.all(8),
+        filled: true,
+        fillColor: const Color.fromRGBO(247, 248, 249, 1),
+        hintText: text,
+        hintStyle: const TextStyle(
+            fontSize: 15,
+            fontFamily: "inter",
+            fontWeight: FontWeight.w500,
+            color: Color.fromRGBO(131, 145, 161, 1)),
+        focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.black),
+            borderRadius: BorderRadius.all(Radius.circular(8))),
+        enabledBorder: const OutlineInputBorder(
+          borderSide: BorderSide(
+            width: 2,
+            color: Color.fromRGBO(218, 218, 218, 1),
+          ),
+          borderRadius: BorderRadius.all(
+            Radius.circular(8),
+          ),
+        ),
+        border: const OutlineInputBorder(
+          borderSide: BorderSide(
+            width: 2,
+            color: Color.fromRGBO(218, 218, 218, 1),
+          ),
+        ),
+        errorBorder: const OutlineInputBorder(
+            borderSide: BorderSide(
+              width: 2,
+              color: Colors.red,
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(8))));
+  }
+
+  dropdownButtonFormField() {
+    return const InputDecoration(
+      focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.black),
+          borderRadius: BorderRadius.all(Radius.circular(8))),
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          width: 2,
+          color: Color.fromRGBO(218, 218, 218, 1),
+        ),
+        borderRadius: BorderRadius.all(
+          Radius.circular(8),
+        ),
+      ),
+    );
+  }
 }
