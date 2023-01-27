eb60022c4eb01bd84632638b7bd87a962bb2eff1
jaehee21232 <a62533196@gmail.com>
2023-01-27 20:33:54 +0900

    feat: 자주 묻는 질문 페이지 간단하게 완성

--- lib/page/asked.dart
+++ lib/page/asked.dart
@@ -1,27 +1,60 @@
+import 'package:code_dc/model/color.dart';
+import 'package:code_dc/model/faq.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
-class AskedPage extends StatelessWidget {
+class AskedPage extends StatefulWidget {
   const AskedPage({super.key});
+
+  @override
+  State<AskedPage> createState() => _AskedPageState();
+}
+
+class _AskedPageState extends State<AskedPage> {
+  List<String> que = Quse().question();
+
+  List<String> ans = Quse().answer();
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return SafeArea(
-      child: SingleChildScrollView(
-          child: Container(
-        height: size.height,
-        width: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "질문 페이지",
-              style: TextStyle(color: Colors.white, fontSize: 30),
-            )
-          ],
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "자주 묻는 질문",
+            style: DCColor().boldFontBlack(28, FontWeight.w800),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.white,
         ),
-      )),
+        body: AnimationLimiter(
+          child: ListView.builder(
+            itemCount: que.length,
+            itemBuilder: (context, index) {
+              return AnimationConfiguration.staggeredList(
+                  position: index,
+                  child: SlideAnimation(
+                    child: FadeInAnimation(
+                      child: ExpansionTile(
+                          title: Text(
+                            que[index],
+                            style: DCColor().boldFontBlack(20, FontWeight.w700),
+                          ),
+                          children: [
+                            ListTile(
+                                title: Text(
+                              ans[index],
+                              style:
+                                  DCColor().boldFontBlack(16, FontWeight.w500),
+                            ))
+                          ]),
+                    ),
+                  ));
+            },
+          ),
+        ),
+      ),
     );
   }
 }
