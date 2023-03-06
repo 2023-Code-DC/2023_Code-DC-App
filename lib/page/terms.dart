6e578cf700aa1d0d5fada22cc11cfe7ecc4dd4ad
jaehee21232 <a62533196@gmail.com>
2023-03-07 02:32:36 +0900

    feat: 약관 페이지 추가

---
+++ lib/page/terms.dart
@@ -0,0 +1,20 @@
+import 'package:code_dc/model/color.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Terms extends StatelessWidget {
+  const Terms({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Text(
+        "코드디씨에서는 \n약관따위는 필요없습니다.",
+        textAlign: TextAlign.center,
+        style: DCColor().boldFontBlack(30, FontWeight.w700),
+      )),
+    );
+  }
+}
